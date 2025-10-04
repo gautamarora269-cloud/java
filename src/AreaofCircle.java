cd3a70188000645d951b29aedc1bf906b19b0e61
gautamarora269 <gautamarora269@gmail.com>
2025-10-04 11:18:15 +0530

    Committing Area of Circle java file

---
+++ src/AreaofCircle.java
@@ -0,0 +1,9 @@
+public class AreaofCircle {
+    public static void main(String[] args) {
+        double radium = 5;
+        double radus = 0;
+        double radius = 0;
+        double area = Math.PI * radus * radius;
+        System.out.println(area);
+    }
+}
