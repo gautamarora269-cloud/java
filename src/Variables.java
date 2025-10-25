00a1cf18e98623b38af61c02ae7a422d507e1562
gautamarora269-cloud <gautamarora269@gmail.com>
2025-10-25 15:38:10 +0530

    Create Variables.java

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "gautam";
+        int age = 30;
+        String neighbour = "priya";
+        String friend = neighbour;
+    }
+}
