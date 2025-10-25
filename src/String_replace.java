e3a9478f0d9b03a02321f5d724993d510306eb82
gautamarora269-cloud <gautamarora269@gmail.com>
2025-10-25 15:39:35 +0530

    Add String_replace class to replace characters

---
+++ src/String_replace.java
@@ -0,0 +1,7 @@
+public class String_replace {
+    public static void main(String[] args) {
+        String name = "Gautam";
+        String name2 = name.replace('G', 'a');
+        System.out.println(name2);
+    }
+}
