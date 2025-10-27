cfadf2572f03ee4367b14c9690595895b32b2338
gautamarora269-cloud <gautamarora269@gmail.com>
2025-10-27 14:55:10 +0530

    Create Explicit_Casting.java

---
+++ src/Explicit_Casting.java
@@ -0,0 +1,7 @@
+public class Explicit_Casting {
+    public static void main(String[] args) {
+        double num = 10.99;
+        int newNum = (int) num;
+        System.out.println(newNum);
+    }
+}
