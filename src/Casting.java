211640daf347dace01a7884fc5296482df773bb3
gautamarora269-cloud <gautamarora269@gmail.com>
2025-10-27 14:52:37 +0530

    Create Casting.java

---
+++ src/Casting.java
@@ -0,0 +1,9 @@
+public class Casting {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        double result = (double) a / b;  // ek ko double banaya
+        System.out.println(result);
+
+    }
+}
