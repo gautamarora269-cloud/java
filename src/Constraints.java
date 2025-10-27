47dfe93aa6eeaae101ed5efe4dc288b627e80358
gautamarora269-cloud <gautamarora269@gmail.com>
2025-10-27 15:18:21 +0530

    Create Constraints.java

---
+++ src/Constraints.java
@@ -0,0 +1,10 @@
+public class Constraints {
+    public static void main(String[] args) {
+        int age = 30;
+        age = 32;
+        age = 33;
+
+        final float PI = 3.14f;
+        System.out.println(PI);
+    }
+}
