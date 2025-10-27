420d6e6733c4b63395d4b01b2543dd7d7f41f690
gautamarora269-cloud <gautamarora269@gmail.com>
2025-10-27 15:37:15 +0530

    Create Aritmatic_operactors.java

---
+++ src/Aritmatic_operactors.java
@@ -0,0 +1,35 @@
+public class Aritmatic_operactors {
+    public static void main(String[] args) {
+        //add
+        int a = 1;
+        int b = 2;
+        int sum = a+b;
+        System.out.println(sum);
+
+        //diff
+        int c = 1;
+        int d = 2;
+        int diff = c-d;
+        System.out.println(diff);
+
+        //multiply
+        int e = 1;
+        int f = 2;
+        int multiply = e*f;
+        System.out.println(multiply);
+
+        //division
+        double  g = 1;
+        double  h = 2;
+        double Div = c/d;
+        System.out.println(Div);
+
+        //Modulo
+        double  i = 1;
+        double  j = 2;
+        double modulo = i%j;
+        System.out.println(modulo);
+
+
+    }
+}
