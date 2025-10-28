d873136fe264f0f2b9d6bdfbae72dcdcbacfc5b2
gautamarora269-cloud <gautamarora269@gmail.com>
2025-10-28 13:10:02 +0530

    Create Comparison_operators.java

---
+++ src/Comparison_operators.java
@@ -0,0 +1,9 @@
+public class Comparison_operators {
+    public static void main(String[] args) {
+        int age = 30;
+        if (age > 18)
+            System.out.println("can vote");
+        else
+            System.out.println("can't vote");
+    }
+}
