bc6b68bf900b9b0eb4d9bada3674238d53875f90
gautamarora269-cloud <gautamarora269@gmail.com>
2025-10-28 13:11:59 +0530

    Create Comparison_operators2.java

---
+++ src/Comparison_operators2.java
@@ -0,0 +1,9 @@
+public class Comparison_operators2 {
+    public static void main(String[] args) {
+        boolean isSunUp = true;
+        if (isSunUp == true)
+            System.out.println("day");
+        else
+            System.out.println("night");
+    }
+}
