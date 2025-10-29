c4757a09f21ba5a1fc1fd6f0657d0288283cdea9
gautamarora269-cloud <gautamarora269@gmail.com>
2025-10-29 11:32:25 +0530

    Create For_each_loop.java

---
+++ src/For_each_loop.java
@@ -0,0 +1,8 @@
+public class For_each_loop {
+    public static void main(String[] args) {
+        String [] browsers = {"Chrome" , "Edge" , "Firefox"};
+        for (String b : browsers) {
+            System.out.println("testing on:" + b);
+        }
+    }
+}
