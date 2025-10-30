d8dc8681db441a304274fd219f2f3fe077a6c6c3
gautamarora269-cloud <gautamarora269@gmail.com>
2025-10-30 12:07:23 +0530

    Create Class.java

---
+++ src/Class.java
@@ -0,0 +1,13 @@
+public class Class {
+    public static void main(String[] args) {
+        class Car {
+            String color;  // variable to store color
+            int speed;     // variable to store speed
+
+            // Method to start the car
+            void start() {
+                System.out.println("Car is starting");
+            }
+        }
+    }
+}
