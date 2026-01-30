5d0635799d3f0054f1256fb3df77b381d50bca54
Gautam Arora <gautamarora269@gmail.com>
2026-01-30 21:46:26 +0530

    Create JavaTypes_Primitive.java

---
+++ JavaTypes_Primitive.java
@@ -0,0 +1,16 @@
+public class JavaTypes_Primitive {
+    public static void main (String[] args) {
+        byte age = 30;
+        int phone_no = 999124980;
+        long phone_no2 = 919991249808L;
+        float marks = 70.2F;
+        boolean pass = true;
+        char full_name = 'G';
+
+        System.out.println("age: " + age);
+        System.out.println("phone_no: " + phone_no);
+        System.out.println("marks:" + marks);
+        System.out.println("pass: " + pass);
+        System.out.println("full_name: " + full_name);
+    }
+}
