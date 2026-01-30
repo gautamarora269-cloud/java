7458ff7747f24d6b135c8b19643c40f511abe7ef
Gautam Arora <gautamarora269@gmail.com>
2026-01-30 22:26:21 +0530

    Create Variables_Addition_Using_Variables.java

---
+++ Variables_Addition_Using_Variables.java
@@ -0,0 +1,11 @@
+public class Variables_Addition_Using_Variables {
+    public static void main(String[] args) {
+        //store two numbers in variables
+        //add them
+
+        String first_name = "Gautam ";
+        String last_name = "Arora";
+        String both = first_name + last_name;
+        System.out.println("Fullname: " + both);
+    }
+}
