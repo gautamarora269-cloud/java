40ab107871559c3adf2d7ea5de5f7277c550ce15
Gautam Arora <gautamarora269@gmail.com>
2026-01-30 22:25:56 +0530

    Create Variable_Questions.java

---
+++ Variable_Questions.java
@@ -0,0 +1,15 @@
+public class Variable_Questions {
+    public static void main(String[] args) {
+
+        //stores your name in a variable
+        //stores your age in a variable
+
+        String name = "gautam";
+        int age = 24;
+
+        System.out.println("name: " + name);
+        System.out.println("age: " + age);
+
+
+    }
+}
