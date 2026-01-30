d4a3a8cd49ea37feccaa6509c5bfb7f9360381f6
Gautam Arora <gautamarora269@gmail.com>
2026-01-30 21:45:20 +0530

    Add Replace class with string replacement example
    Implement a simple string replacement in main method.

---
+++ Replace.java
@@ -0,0 +1,11 @@
+public class Replace {
+    public static void main(String[] args) {
+
+        //replace
+        String name = "gautam";
+        String name2 = name.replace('a', 'e');
+
+        System.out.println(name2);
+        System.out.println(name);
+    }
+}
