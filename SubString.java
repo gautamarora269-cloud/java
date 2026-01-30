0d6128603f1a3e7f0fb0ec72e6992b8b0499abc1
Gautam Arora <gautamarora269@gmail.com>
2026-01-30 21:45:40 +0530

    Create SubString.java

---
+++ SubString.java
@@ -0,0 +1,9 @@
+public class SubString {
+    public static void main(String[] args) {
+
+        //SubString
+        String name = "gautam and arora";
+
+        System.out.println (name.substring(0,6));
+    }
+}
