da4878375bd249bfe4dbdc1ec72b5d8bea0c1f9f
Gautam Arora <gautamarora269@gmail.com>
2026-01-30 22:25:35 +0530

    Create Variable_name.java

---
+++ Variable_name.java
@@ -0,0 +1,11 @@
+public class Variable_name {
+    public static void main (String[] args) {
+        //Hello Gautam, your age is 24
+
+        String name = "hello gautam";
+        int age = 24;
+
+        System.out.println(name + ", your age is " + age);
+
+    }
+}
