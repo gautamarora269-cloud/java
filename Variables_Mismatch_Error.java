b86161cbc96fae21e00b24f0ceabdcd1dd8fc8e6
Gautam Arora <gautamarora269@gmail.com>
2026-01-30 22:27:11 +0530

    Create Variables_Mismatch_Error.java

---
+++ Variables_Mismatch_Error.java
@@ -0,0 +1,7 @@
+public class Variables_Mismatch_Error {
+    public static void main (String[] args) {
+        int price = 99;
+        int total = price;
+        System.out.println (total);
+    }
+}
