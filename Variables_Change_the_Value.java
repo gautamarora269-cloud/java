c219a91d82585cbaeea023fce5108dee90194e91
Gautam Arora <gautamarora269@gmail.com>
2026-01-30 22:26:38 +0530

    Create Variables_Change_the_Value.java

---
+++ Variables_Change_the_Value.java
@@ -0,0 +1,15 @@
+public class Variables_Change_the_Value {
+    public static void main(String[] args) {
+
+        //int marks = 50;
+        //print marks
+        //change marks to 75
+        //print again
+
+        int marks = 50;
+        System.out.println("marks: " + marks);
+
+        marks = 75;
+        System.out.println("marks: " + marks);
+    }
+}
