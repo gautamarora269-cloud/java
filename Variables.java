69bb3dc40a26b7e3cde3f34abf70d4398de17a63
Gautam Arora <gautamarora269@gmail.com>
2026-01-30 21:46:01 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main (String[] args) {
+        //variables
+        String name = "Gautam";
+        int age = 24;
+        String course = "Mca";
+        int year =  2025;
+
+        System.out.println("Name: " + name);
+        System.out.println("age: "+ age);
+        System.out.println("Course: "+ course);
+        System.out.println("year: "+ year);
+    }
+}
