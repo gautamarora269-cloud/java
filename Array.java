1db91b2d62243922e7be983799eb462c6c9b5453
Gautam Arora <gautamarora269@gmail.com>
2026-02-02 11:51:37 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main (String[] args) {
+
+        //array
+        int [] number = {10,20,30,40,50,60};
+
+        System.out.println(number[0]);
+        System.out.println(number[2]);
+    }
+}
+
