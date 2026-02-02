ecf3d943e712e263f174c24feb74dc3544d876a9
Gautam Arora <gautamarora269@gmail.com>
2026-02-02 12:32:58 +0530

    Create Array_first_and_last.java

---
+++ Array_first_and_last.java
@@ -0,0 +1,8 @@
+public class Array_first_and_last {
+    public static void main (String[] args) {
+        int[] number = {1,2,3,4,5};
+        System.out.println(number[0]);
+        System.out.println(number[4]);
+
+    }
+}
