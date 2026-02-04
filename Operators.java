e4ae96af6d6c99caa87e610d361e281277066d09
Gautam Arora <gautamarora269@gmail.com>
2026-02-04 08:31:13 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,34 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        //Add
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(sum);
+
+        //Multiply
+        int c = 10;
+        int d = 20;
+        int multiply = c * d;
+        System.out.println(multiply);
+
+        //minus
+        int e = 10;
+        int f = 20;
+        int diff = e - f;
+        System.out.println(diff);
+
+        //Divide
+        int g = 10;
+        int h = 20;
+        int divide = g - h;
+        System.out.println(divide);
+
+        //modulo (reminder)
+        int i = 10;
+        int j = 20;
+        int modulo = i % j;
+        System.out.println(modulo);
+    }
+}
