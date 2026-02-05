47291479028ed4a17cb162cb4c3e11dc9d0143f4
Gautam Arora <gautamarora269@gmail.com>
2026-02-05 13:37:45 +0530

    Create Maths.java

---
+++ Maths.java
@@ -0,0 +1,13 @@
+public class Maths {
+    public static void main (String[] args) {
+
+        //max (5,6)
+        System.out.println(Math.max(5,6));
+
+        //min (5,6)
+        System.out.println(Math.min(5,6));
+
+        //random
+        System.out.println(Math.random());
+    }
+}
