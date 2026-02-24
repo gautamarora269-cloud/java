c28732b46742f5b8497761e510faba2fa119f47d
Gautam Arora <gautamarora269@gmail.com>
2026-02-24 14:53:27 +0530

    Create Print.java

---
+++ Print.java
@@ -0,0 +1,12 @@
+public class Print {
+    public static void main(String[] args) {
+
+        System.out.print(10+20);
+        System.out.print(11111);
+        System.out.print("hello world");
+        System.out.print(10.2);
+        System.out.print('a');
+        System.out.print(true);
+
+    }
+}
