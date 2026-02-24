c44d0618109e3e60070fff8ead878a6fb30e9e57
Gautam Arora <gautamarora269@gmail.com>
2026-02-24 14:54:06 +0530

    Create Line_Oriented.java

---
+++ Line_Oriented.java
@@ -0,0 +1,9 @@
+public class Line_Oriented {
+    public static void main(String[] args) {
+        //printing hello world
+        System.out.println("Hello World");
+    } //end of main
+} //end of class
+
+
+
