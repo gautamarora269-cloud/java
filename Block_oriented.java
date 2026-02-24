5ec093b0f25ca56e8e62f8245ebd019f34356373
Gautam Arora <gautamarora269@gmail.com>
2026-02-24 14:54:57 +0530

    Create Block_oriented.java

---
+++ Block_oriented.java
@@ -0,0 +1,12 @@
+public class Block_oriented {
+    public static void main(String[] args) {
+        //printing hello world
+        System.out.println("Hello World");
+
+        /*System.out.print("block");
+        System.out.print("oriented");
+        System.out.print("comment");
+         */
+
+    } //end of main
+}
