3c30704c88a3ffd9e74c065222d5bf5b70ff9cae
Gautam Arora <gautamarora269@gmail.com>
2026-02-24 14:53:08 +0530

    Create Println.java

---
+++ Println.java
@@ -0,0 +1,12 @@
+public class Println {
+    public static void main(String[] args) {
+                System.out.println(10+20);
+                System.out.println(11111);
+                System.out.println("hello world");
+                System.out.println(10.2);
+                System.out.println('a');
+                System.out.println(true);
+
+            }
+        }
+        
