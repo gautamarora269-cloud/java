32303dee67d1c997353c0d52777d0875f932d8f8
Gautam Arora <gautamarora269@gmail.com>
2026-02-24 14:52:09 +0530

    Create Type.java

---
+++ Type.java
@@ -0,0 +1,20 @@
+public class Type {
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b = (double) a;
+        System.out.println(b);
+
+        char c = 'a';
+        int d = (int) c;
+        System.out.println(d);
+
+        int e = 65;
+        char f = (char) e;
+        System.out.println(f);
+
+        double g = 30.0;
+        int k = (int) g;
+        System.out.println(k);
+    }
+}
