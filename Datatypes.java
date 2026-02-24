3879035e1f0d5bf264de1da3175c55f0e34f0c47
Gautam Arora <gautamarora269@gmail.com>
2026-02-24 14:54:23 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,24 @@
+public class Datatypes {
+    public static void main (String[] args) {
+        byte l = 1;
+        short c = 2;
+        int d = 34;
+        long e = 34567l;
+
+        float f = 2.5f;
+        double h = 2.33d;
+
+        char p = 'a';
+        boolean w = true;
+
+        System.out.println(e);
+        System.out.println(l);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(h);
+        System.out.println(p);
+        System.out.println(w);
+
+    }
+}
