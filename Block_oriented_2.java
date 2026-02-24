a8972c0428ff6c73cf0385e5248a121365fc4666
Gautam Arora <gautamarora269@gmail.com>
2026-02-24 14:54:42 +0530

    Create Block_oriented_2.java

---
+++ Block_oriented_2.java
@@ -0,0 +1,12 @@
+public class Block_oriented_2 {
+    public static void main(String[] args) {
+        //printing sum of 5 and 9
+
+        /*System.out.print(5//=9);
+        System.out.print(10;
+        System.out.print(20);
+         */
+
+    } //end of main
+
+}
