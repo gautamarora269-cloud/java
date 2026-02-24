63af3d33a2f9f7ef0887a01fc1131d5259c9beb9
Gautam Arora <gautamarora269@gmail.com>
2026-02-24 14:52:46 +0530

    Add Var class with main method for byte examples

---
+++ Var.java
@@ -0,0 +1,12 @@
+public class Var {
+    public static void main(String[] args) {
+        //multi
+        byte a ;
+        a = 1;
+        System.out.println(a);
+
+        //single
+        byte b = 2;
+        System.out.println(b);
+    }
+}
