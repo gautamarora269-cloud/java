9bf378b7241d079b22679cfda08fb4338cdb3baf
Gautam Arora <gautamarora269@gmail.com>
2026-02-24 14:53:48 +0530

    Create Line_oriented_2.java

---
+++ Line_oriented_2.java
@@ -0,0 +1,8 @@
+public class Line_oriented_2 {
+    public static void main(String[] args) {
+        //printing sum of 5 and 9
+        System.out.print(5+9);
+        //System.out.println(10)
+
+    } //end of main
+} //end of class
