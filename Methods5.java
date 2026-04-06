5f97425dbc5fa4d8fe4a16163f664cb4aae1f0b8
Gautam Arora <gautamarora269@gmail.com>
2026-04-06 15:37:24 +0530

    Create Methods5.java

---
+++ Methods5.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Program5 {
+    public static void main(String[] args) {
+        int result = square(5);
+        System.out.println("Square is: " + result);
+    }
+        public static int square(int num) {
+        return num*num;
+        }
+    }
