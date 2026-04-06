9289221d4ee839cfce3ebb66c516619667bd6935
Gautam Arora <gautamarora269@gmail.com>
2026-04-06 15:38:26 +0530

    Create Methods9.java

---
+++ Methods9.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Program9 {
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        System.out.println("nothing");
+    }
+}
