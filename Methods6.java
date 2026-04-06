410a35dd8d453da3424216877687ed785e9626a7
Gautam Arora <gautamarora269@gmail.com>
2026-04-06 15:37:38 +0530

    Create Methods6.java

---
+++ Methods6.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class Program6 {
+    //Create a method without return type{
+    public static void main(String[] args) {
+        square(5);
+}
+public static void square(int num) {
+        int result = num * num;
+        System.out.println("Square is: " + result);
+    }
+}
