2887e96e217bca54fd5279a02441983591cf2255
Gautam Arora <gautamarora269@gmail.com>
2026-04-06 15:38:38 +0530

    Create Methods10.java

---
+++ Methods10.java
@@ -0,0 +1,10 @@
+package Methods;
+
+public class Program10 {
+    public static void main(String[] args) {
+        sleep();
+    }
+    public static void sleep() {
+        System.out.println("nothing");
+    }
+}
