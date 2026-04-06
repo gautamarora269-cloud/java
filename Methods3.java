05d9a2a93ecee54e32cc9a6e063f124fb2d588cc
Gautam Arora <gautamarora269@gmail.com>
2026-04-06 15:36:51 +0530

    Create Methods3.java

---
+++ Methods3.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class Program3 {
+    public static void main(String[] args) {
+        addNumbers();
+    }
+    //Create a method to add two numbers
+    public static void addNumbers() {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("c = " + c);
+    }
+}
