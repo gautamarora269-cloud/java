2d2694bd04abab7d311ea2f14202ade7dc3ade49
Gautam Arora <gautamarora269@gmail.com>
2026-04-06 15:36:33 +0530

    Create Methods2.java

---
+++ Methods2.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Program2 {
+    //Create a method to print your name
+    public static void main(String[] args) {
+        Name();
+    }
+    public static void Name() {
+        System.out.println("Gautam");
+    }
+}
