6a3b840a6bd11796943116949186410cb8fb767d
Gautam Arora <gautamarora269@gmail.com>
2026-04-06 15:38:11 +0530

    Create Methods8.java

---
+++ Methods8.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Program8 {
+    public static void main(String[] args) {
+        drive();
+    }
+            public static void drive () {
+            System.out.println("nothing");
+        }
+        }
+
