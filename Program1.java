de377e279629a497559eadf00963aa1ea81a0243
Gautam Arora <gautamarora269@gmail.com>
2026-04-06 15:36:11 +0530

    Program1.java

---
+++ Program1.java
@@ -0,0 +1,17 @@
+package Methods;
+
+public class Program1 {
+    public static void main(String[] args) {
+        System.out.println("main start");
+        m1();
+        m2();
+    }
+    //m1 method
+    public static void m1() {
+        System.out.println("m1 method");
+    }
+    //m2 method
+    public static void m2() {
+        System.out.println("m2 method");
+    }
+}
