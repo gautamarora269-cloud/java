2575d0712b3432b5ce3094653a72a8c2ef929284
Gautam Arora <gautamarora269@gmail.com>
2026-04-06 15:37:53 +0530

    Create Metyhods7.java

---
+++ Metyhods7.java
@@ -0,0 +1,16 @@
+package Methods;
+
+public class Program7 {
+    //create a add method which accepts no arguement and return nothing
+    public static void main(String[] args) {
+        add();
+    }
+    public static void add() {
+        int a = 12;
+        int b = 13;
+        int sum = a + b;
+        System.out.println(sum);
+
+        }
+    }
+
